bf120a8a218b5b418f5863d03312f8317bf6df40
Subn0rmalUSMl3n0 <Rruz@usm.cl>
2023-12-03 15:12:37 -0300

    Ajustados cambios, agregados plugins mapas

--- proy/comentarios/views.py
+++ proy/comentarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Comentario
 from .forms import ComentarioForm
 import folium
-from folium.plugins import Geocoder
+from folium.plugins import Geocoder, LocateControl, MarkerCluster
 from folium.features import LatLngPopup, ClickForLatLng
 min_lon, max_lon = -76, -65
 min_lat, max_lat = -56, -17    
@@ -20,23 +20,21 @@ def listar_comentarios(request):
                             min_lon=min_lon,
                             max_lon=max_lon,
                             ) 
-       #BOUNDS MAPA
-    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(m)
-    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(m)
-    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(m)
-    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(m)
-    # Agregar marcadores para cada comentario
+
+    #PLUGINS Y DEMAS
+    folium.plugins.LocateControl(auto_start=False).add_to(m)
+    folium.plugins.Geocoder().add_to(m)
+    marker_cluster = MarkerCluster().add_to(m)
+
+    # Agregar marcadores para cada comentario al clúster de marcadores
     for comentario in comentarios:
         try:
             lat, lon = map(float, comentario.ubicacion.strip("[]").split(","))
             popup_content = f"{comentario.autor}: {comentario.contenido}"
-            folium.Marker(location=[lat, lon], popup=popup_content).add_to(m)
+            folium.Marker(location=[lat, lon], popup=popup_content).add_to(marker_cluster)
         except ValueError:
             # Manejar errores si la ubicación no está en el formato correcto
             pass
-
-    #PLUGINS Y DEMAS
-    folium.plugins.Geocoder().add_to(m)
     context = {'comentarios': comentarios, 'mapa': m._repr_html_()}
     return render(request, 'comentarios/listar_comentarios.html', context)
 
@@ -63,21 +61,17 @@ def agregar_comentario(request):
                             min_lon=min_lon,
                             max_lon=max_lon,
                             ) 
-       #BOUNDS MAPA
-    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(m)
-    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(m)
-    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(m)
-    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(m)
+
     
     #PLUGINS Y DEMAS
     folium.plugins.Geocoder().add_to(m)    
+    folium.plugins.LocateControl(auto_start=False).add_to(m)
 
     lat_lng_popup = LatLngPopup()
     m.add_child(lat_lng_popup)
     
     click_for_lat_lng = ClickForLatLng(format_str = '"[" + lat + "," + lng +"]"', alert= True)
     m.add_child(click_for_lat_lng)
-    
     context = {'form': form, 'mapap': m._repr_html_()}
     return render(request, 'comentarios/agregar_comentario.html', context)
 
--- proy/mapap/views.py
+++ proy/mapap/views.py
@@ -2,67 +2,39 @@
 from django.shortcuts import render
 import folium
 import pandas as pd
-import folium
 from django.conf import settings
 from shapely import wkt
 from shapely.geometry import Polygon
 from folium.features import ClickForLatLng
-from folium.plugins import Geocoder
+from folium.plugins import Geocoder, LocateControl
 
 min_lon, max_lon = -76, -65
 min_lat, max_lat = -56, -17
 
 
 def create_polygon_from_wkt(wkt_string):
-    # Crea una geometría de tipo Polygon a partir de una cadena WKT
     geom = wkt.loads(wkt_string)
     if geom.geom_type == 'LineString':
-        # Si la geometría es LineString, cierra el bucle para formar un polígono
         geom = Polygon(geom)
     return geom
 
-def cargar_y_mostrar_csv(request, csv_file_path, i):
-    # Leer datos CSV con pandas
+def cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords):    # Leer datos CSV con pandas
     df = pd.read_csv(csv_file_path)
-    if i == 0:
-        popi = [-36.552302, -71.889792]
-    elif i == 1:
-        popi = [-22.958393,-68.653564]
-    elif i == 2:
-        popi = [-18.719097,-70.246582]
-    elif i == 3:
-        popi = [-30.666266,-70.532227]
-    elif i == 4:
-        popi = [-41.071069,-73.245850]
-    elif i == 5:
-        popi = [-41.525030,-71.949463]
-    elif i == 6:
-        popi = [-43.524655,-74.190674]
-    elif i == 7:
-        popi = [-38.933776,-72.026367]
-    elif i == 8:
-        popi = [-40.738933,-72.872314]
-    elif i == 9:
-        popi = [-42.354485,-73.894043]
-    elif i == 10:
-        popi = [-33.174426,-70.762937]
-    elif i == 11:
-        popi = [-23.160563,-67.445068]
-    elif i == 12:
-        popi = [-45.151053,-72.333984]
+
     
     # Crear mapa
-    m= folium.Map(location = popi,
-                            zoom_start= 2,
-                            zoom_control=False,
-                            max_bounds=True,
-                            min_zoom=6.5,
-                            tiles= "cartodb positron",
-                            min_lat=min_lat,
-                            max_lat=max_lat,
-                            min_lon=min_lon,
-                            max_lon=max_lon,
-                            )
+    m = folium.Map(
+        location=initial_coords,
+        zoom_start=2,
+        zoom_control=False,
+        max_bounds=True,
+        min_zoom=6.5,
+        tiles="cartodb positron",
+        min_lat=min_lat,
+        max_lat=max_lat,
+        min_lon=min_lon,
+        max_lon=max_lon,
+    )
     # Iterar sobre las filas y manejar errores en la conversión de geometrías
     for _,row in df.iterrows():
             # Obtener la geometría en formato WKT desde la columna "WKT"
@@ -79,94 +51,107 @@ def cargar_y_mostrar_csv(request, csv_file_path, i):
 
     #PLUGINS
     folium.plugins.Geocoder().add_to(m)    
+    folium.plugins.LocateControl(auto_start=False).add_to(m)
 
     lat_lng_popup = ClickForLatLng(format_str = '"[" + lat + "," + lng +"]"', alert=False)
     m.add_child(lat_lng_popup)
-    # Convertir el mapa a HTML
+            
     m_html = m._repr_html_()
-
     m_datos = {"m" + str(i) + "_html": m_html}
-    print('mapas/mapa' + str(i) + '.html')
-    print('m' + str(i) + '_html')
-    return render(request, 'mapas/mapa' + str(i) + '.html', {'m' + str(i) + '_datos': m_datos})
+    return render(request,f'mapas/mapa{i}.html',{f'm{i}_datos': m_datos})
+
 
 def mapa0(request):
+    # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'AAAAA BASE.csv'
-    i=0
+    i = 0
+    initial_coords = [-36.552302, -71.889792]
     # Llamar a la función para cargar y mostrar el CSV en el mapa
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa1(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Alpaca.csv'
-    i=1
-    # Llamar a la función para cargar y mostrar el CSV en el mapa
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 1
+    initial_coords = [-22.958393, -68.653564]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa2(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Chuncho del Norte.csv'
-    i=2
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 2
+    initial_coords = [-18.719097, -70.246582]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa3(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Cóndor.csv'
-    i=3
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 3
+    initial_coords = [-30.666266, -70.532227]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa4(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Fiu Fiu.csv'
-    i=4
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 4
+    initial_coords = [-41.071069, -73.245850]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa5(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Lanturn.csv'
-    i=5
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 5
+    initial_coords = [-41.525030, -71.949463]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa6(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Lobo marino.csv'
-    i=6
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 6
+    initial_coords = [-43.524655, -74.190674]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa7(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Monito del monte.csv'
-    i=7
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 7
+    initial_coords = [-38.933776, -72.026367]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa8(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Murcielago Orejon.csv'
-    i=8
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 8
+    initial_coords = [-40.738933, -72.872314]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa9(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Pudú.csv'
-    i=9
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 9
+    initial_coords = [-42.354485, -73.894043]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa10(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Puma.csv'
-    i=10
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 10
+    initial_coords = [-33.174426, -70.762937]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
+
 def mapa11(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Vicuña.csv'
-    i=11
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 11
+    initial_coords = [-23.160563, -67.445068]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 def mapa12(request):
     # Ruta al archivo CSV
     csv_file_path = settings.BASE_DIR / 'mapap' / 'csv_files' / 'Zorro gris.csv'
-    i=12
-    return cargar_y_mostrar_csv(request, csv_file_path,i)
+    i = 12
+    initial_coords = [-45.151053, -72.333984]
+    return cargar_y_mostrar_csv(request, csv_file_path, i, initial_coords)
 
 
 
