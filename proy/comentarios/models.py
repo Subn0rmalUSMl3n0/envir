cca1344869003253c539c327659465542a50d024
bobal <bobalazo@gmail.com>
2023-12-05 23:36:02 -0300

    rama actualizada

--- proy/comentarios/models.py
+++ proy/comentarios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Comentario(models.Model):
     autor = models.CharField(max_length=100)
     ubicacion = models.CharField(help_text='(Favor pegar sin cambios desde el portapapeles)',max_length=140)
-    contenido = models.CharField(max_length= 21381,help_text='Explique que ocurrió en su avistamiento')
+    contenido = models.CharField(max_length= 1000000000)
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.autor
